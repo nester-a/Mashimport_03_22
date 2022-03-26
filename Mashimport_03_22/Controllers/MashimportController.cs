2acf3f8d7ed2158a2e0e32ae80fb584b6dc69586
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-26 16:27:51 +0300

    Внедрение зависимостей в контроллеры

--- Mashimport_03_22/Controllers/MashimportController.cs
+++ Mashimport_03_22/Controllers/MashimportController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mashimport_03_22.Models;
+using Mashimport_03_22.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mashimport_03_22.Controllers
 {
     public abstract class MashimportController : Controller
     {
+        private readonly IContactsData contacts;
+        private readonly ILogger<MashimportController> logger;
+        protected ContactsInfo contactsInfo;
+
+        public MashimportController(IContactsData contacts, ILogger<MashimportController> logger)
+        {
+            this.contacts = contacts;
+            this.logger = logger;
+        }
         public IActionResult About() => View();
         public IActionResult Partners() => View();
-        public IActionResult Contact() => View();
+        public IActionResult Contact() => View(contactsInfo);
     }
 }
--- Mashimport_03_22/Controllers/RussianController.cs
+++ Mashimport_03_22/Controllers/RussianController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mashimport_03_22.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mashimport_03_22.Controllers
 {
     public class RussianController : MashimportController
     {
+        public RussianController(IContactsData contacts, ILogger<MashimportController> logger) : base(contacts, logger)
+        {
+            contactsInfo = contacts.GetById(0);
+        }
     }
 }
