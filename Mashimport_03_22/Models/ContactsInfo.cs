cc4bf821f0bb1a074b21f2291b82cb245986b7c2
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-26 16:07:16 +0300

    Тестовые данные для работы

--- Mashimport_03_22/Models/ContactsInfo.cs
+++ Mashimport_03_22/Models/ContactsInfo.cs
@@ -2,6 +2,7 @@
 {
     public class ContactsInfo
     {
+        public int Id { get; set; }
         public string Address { get; set; } = string.Empty;
         public string TelephoneNumber { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
--- Mashimport_03_22/ViewModels/ContactsInfoViewModel.cs
+++ Mashimport_03_22/ViewModels/ContactsInfoViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class ContactsInfoViewModel
     {
+        public int Id { get; set; }
         public string Address { get; set; } = string.Empty;
         public string TelephoneNumber { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
