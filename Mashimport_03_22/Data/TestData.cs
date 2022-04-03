28b0dba738e56b387cfe9b4a5afa6c0b52f005e2
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-03 15:41:51 +0300

    Удаление лишних классов

--- Mashimport_03_22/Data/TestData.cs
+++ Mashimport_03_22/Data/TestData.cs
@@ -1,4 +1,5 @@
 ﻿using Mashimport_03_22.Models;
+using Mashimport_03_22.Services;
 
 namespace Mashimport_03_22.Data
 {
@@ -11,19 +12,19 @@ namespace Mashimport_03_22.Data
             new ContactsInfo() { Id = 1, Address = "107023, Москва, ул. Большая Семеновская, д.40", Email = email, TelephoneNumber = telephoneNumber },
             new ContactsInfo() { Id = 2, Address = "40, Bolshaya Semenovskaya, Moscow, 107023", Email = email, TelephoneNumber = telephoneNumber },
         };
-        public static MenuItems Rus { get; } = new()
+        public static MenuData Rus { get; } = new()
         {
             About = "О компании",
             Partners = "Партнёры",
             Contact = "Контакты",
         };
-        public static MenuItems Eng { get; } = new()
+        public static MenuData Eng { get; } = new()
         {
             About = "About",
             Partners = "Partners",
             Contact = "Contact",
         };
-        public static MenuItems Chn { get; } = new()
+        public static MenuData Chn { get; } = new()
         {
             About = "关于我们",
             Partners = "合作伙伴",
